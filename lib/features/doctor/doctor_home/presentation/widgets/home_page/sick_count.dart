490ac0b505c18a29b16876fcbe263651cae54b38
Mu-abdullah <moabdullah588@gmail.com>
2026-08-22 04:27:56 +0300

    change home ui

--- lib/features/doctor/doctor_home/presentation/widgets/home_page/sick_count.dart
+++ lib/features/doctor/doctor_home/presentation/widgets/home_page/sick_count.dart
@@ -1,13 +1,12 @@
-import 'package:main_app/core/extextions/extentions.dart';
-
 import 'package:flutter/material.dart';
 import 'package:hugeicons/hugeicons.dart';
+import 'package:main_app/core/extextions/extentions.dart';
 
 import '../../../../../../core/language/lang_keys.dart';
 import '../../../../../../core/routes/routes_name.dart';
 import '../../../../../../core/style/color/app_color.dart';
-import '../../../../../../core/style/statics/app_statics.dart';
 import '../../../../../../core/style/custom_widgets/custom_bottom_sheet.dart';
+import '../../../../../../core/style/statics/app_statics.dart';
 import '../../../../new_patient/presentation/views/new_patient.dart';
 import 'home_count_card.dart';
 
@@ -21,32 +20,42 @@ class SickCount extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         spacing: 16,
-        children: [
-          _buildNextVisitCard(context),
-          _buildStatusRow(context),
-          _buildPatientsRow(context),
-        ],
+        children: [_buildStatusRow(context)],
       ),
-    );
-  }
-
-  Widget _buildNextVisitCard(BuildContext context) {
-    return HomeCountCard(
-      onTap: () => context.pushNamed(RoutesNames.patientScreen),
-      cardIcon: HugeIcons.strokeRoundedNext,
-      cardTitle: LangKeys.nextVisit,
-      cardCount: "",
-      isHighlighted: true,
-      color: AppColors.blueAccent,
-      textColor: AppColors.white,
     );
   }
 
   Widget _buildStatusRow(BuildContext context) {
     return Row(
-      spacing: 10,
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
+        _buildCountCard(
+          context: context,
+          onTap: () => _showNewPatientSheet(context),
+          icon: HugeIcons.strokeRoundedAddTeam,
+          title: LangKeys.registerNewPatient,
+          count: "",
+          color: AppColors.green,
+          textColor: AppColors.white,
+        ),
+        _buildCountCard(
+          context: context,
+          onTap: () => context.pushNamed(RoutesNames.patientListScreen),
+          icon: HugeIcons.strokeRoundedUserGroup,
+          title: LangKeys.patients,
+          count: "135",
+          color: AppColors.grey,
+          textColor: AppColors.white,
+        ),
+        _buildCountCard(
+          context: context,
+          onTap: () => context.pushNamed(RoutesNames.patientScreen),
+          icon: HugeIcons.strokeRoundedUserGroup,
+          title: LangKeys.nextVisit,
+          count: "",
+          color: AppColors.blueAccent,
+          textColor: AppColors.white,
+        ),
         _buildCountCard(
           context: context,
           onTap: () => context.pushNamed(RoutesNames.visitsScreen),
@@ -63,33 +72,6 @@ class SickCount extends StatelessWidget {
           title: LangKeys.completed,
           count: "20",
           color: AppColors.black,
-          textColor: AppColors.white,
-        ),
-      ],
-    );
-  }
-
-  Widget _buildPatientsRow(BuildContext context) {
-    return Row(
-      spacing: 10,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        _buildCountCard(
-          context: context,
-          onTap: () => context.pushNamed(RoutesNames.patientListScreen),
-          icon: HugeIcons.strokeRoundedUserGroup,
-          title: LangKeys.patients,
-          count: "135",
-          color: AppColors.grey,
-          textColor: AppColors.white,
-        ),
-        _buildCountCard(
-          context: context,
-          onTap: () => _showNewPatientSheet(context),
-          icon: HugeIcons.strokeRoundedAddTeam,
-          title: LangKeys.registerNewPatient,
-          count: "",
-          color: AppColors.green,
           textColor: AppColors.white,
         ),
       ],
@@ -113,7 +95,7 @@ class SickCount extends StatelessWidget {
       isHighlighted: true,
       color: color,
       textColor: textColor,
-      width: context.width(percent: 0.46),
+      width: context.width(percent: 0.17),
     );
   }
 
